842341e16b12f541299178bc6a7e95b005f8b215
Eugene Kapombe <kapombek@gmail.com>
2025-04-22 12:03:12 +0300

    changes saved

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
+
 export default function Calculator() {
 
-    return (
-        <div>
-            
-        </div>
-    );
+  useState('') 
 
- };
+
+  fetch(
+    "https://body-mass-index-bmi-calculator.p.rapidapi.com/metric?weight=${}&height= ${}"
+  )
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+
+  return (
+    <div>
+      <h2>BMI Calculator</h2>
+      <h4>Enter your weight and height to get your current BMI</h4>
+      <form>
+        <input type="number" placeholder="Enter your height(cm)" name="height"/>
+        <input type="number" placeholder="Enter your weight(kg)" name="weight"/>
+        <button type="submit">GET BMI!</button>
+      </form>
+    </div>
+  );
+}
